4809d221e14ebed0a552b987c61868248a0b21c7
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-11-17 17:03:59 +0300

    add

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import axios from "axios";
 
 function App() {
+  function fetchData() {
+    const response = axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Hello API</Text>
-      <StatusBar style="auto" />
+      <Button title="Fetch Data" onPress={fetchData} />
     </View>
   );
 }
